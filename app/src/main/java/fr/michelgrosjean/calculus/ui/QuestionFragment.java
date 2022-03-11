b875e212a7e917745f6a0af0d469f8f572b08cde
SoleneMG <solene.michelgrosjean@gmail.com>
2022-03-11 16:28:38 +0100

    init view on question fragment

--- app/src/main/java/fr/michelgrosjean/calculus/ui/QuestionFragment.java
+++ app/src/main/java/fr/michelgrosjean/calculus/ui/QuestionFragment.java
@@ -2,14 +2,24 @@ package fr.michelgrosjean.calculus.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 import fr.michelgrosjean.calculus.R;
 
-public class QuestionFragment extends Fragment {
+public class QuestionFragment extends Fragment implements View.OnClickListener{
+    private TextView txtQuestion;
+    private List<Button> buttons;
 
     public QuestionFragment() {
         super(R.layout.question_fragment);
@@ -17,6 +27,35 @@ public class QuestionFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        initView();
+    }
+
+    private void initView(){
+        txtQuestion = Objects.requireNonNull((getView())).findViewById(R.id.question);
+        Button answer1 = getView().findViewById(R.id.answer1);
+        Button answer2 = getView().findViewById(R.id.answer2);
+        Button answer3 = getView().findViewById(R.id.answer3);
+        Button answer4 = getView().findViewById(R.id.answer4);
+        answer1.setOnClickListener(this);
+        answer2.setOnClickListener(this);
+        answer3.setOnClickListener(this);
+        answer4.setOnClickListener(this);
+        buttons = new ArrayList<>(Arrays.asList(answer1, answer2, answer3, answer4));
+        txtQuestion.setText("test");
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast toast = null;
+        if (view.getId() == R.id.answer1) {
+            toast = Toast.makeText(getContext(), "1", Toast.LENGTH_SHORT);
+        } else if (view.getId() == R.id.answer2) {
+            toast = Toast.makeText(getContext(), "2", Toast.LENGTH_SHORT);
+        } else if (view.getId() == R.id.answer3) {
+            toast = Toast.makeText(getContext(), "3", Toast.LENGTH_SHORT);
+        } else if (view.getId() == R.id.answer4) {
+            toast = Toast.makeText(getContext(), "4", Toast.LENGTH_SHORT);
+        }
+        Objects.requireNonNull(toast).show();
     }
 }
